45c7838e595c9a7dbe6d3b1adf182f8a8e58a21d
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-27 23:29:09 +0200

    created an API file for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,8 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
-  const [storage, useStorage] = useState(null)
-
-  useEffect(() => {
-    fetch('https://podcast-api.netlify.app')
-    .then(response => response.json())
-    .then(data => data)
-  }, [])
-
   return (
-  
+  <h1>hy</h1>
   )
 
 
