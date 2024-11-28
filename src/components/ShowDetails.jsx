c0c7a33a8d9698557ee26df97a4caaf638c5fde2
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-28 14:12:02 +0200

    Fixed my imports for show details

--- src/components/ShowDetails.jsx
+++ src/components/ShowDetails.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-dom/client';
+import { useParams } from 'react-router-dom';
+
 
 const ShowDetails = () => {
   const { showId } = useParams();
