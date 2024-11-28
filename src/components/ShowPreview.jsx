c36a98c547cb95357c49a4e14c20bf8b63fae554
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-28 13:02:44 +0200

    Fixed the image for my show preview file

--- src/components/ShowPreview.jsx
+++ src/components/ShowPreview.jsx
@@ -22,7 +22,7 @@ const ShowPreview = () => {
         <div key={show.id}>
           <h2>{show.title}</h2>
           <p>{show.description}</p>
-          <img src={show.thumbnail} alt={show.title} />
+          <img src={show.image} alt={show.title} />
         </div>
       ))}
     </div>
