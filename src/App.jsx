266ebad18bbe4d9e10ea9332ae0e3f6162bc4505
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-28 10:42:55 +0200

    Fetched show genre data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import './App.css'
-import {fetchShowPreviews} from "./api.js"
 
 function App() {
   return (
-  <Api />
   )
 
 
--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
   export const fetchShowPreviews = async () => {
   try {
-    const response = await fetch('https://podcast-api.netlify.app');
+    const response = await fetch("https://podcast-api.netlify.app");
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Could not fetch show previews:', error);
+    console.error("Could not fetch show previews:", error);
     throw error;
   }
 };
+
+export const fetchShowGenreData = async () => {
+  try{
+    const response = await fetch("https://podcast-api.netlify.app/genre/<ID>");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Could not fetch data for show genres:", error)
+    throw error;
+  }
+  }
