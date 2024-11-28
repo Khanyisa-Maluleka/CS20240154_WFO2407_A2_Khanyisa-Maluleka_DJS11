4bc0f9dfbf95099e14ad9a61a0266ca68578c2ad
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-28 11:23:37 +0200

    Fetched show details data IDs

--- src/api.js
+++ src/api.js
@@ -9,9 +9,9 @@
   }
 };
 
-export const fetchShowGenreData = async () => {
+export const fetchShowGenreData = async (genreId) => {
   try{
-    const response = await fetch("https://podcast-api.netlify.app/genre/<ID>");
+    const response = await fetch(`https://podcast-api.netlify.app/genre/${genreId}/`);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -19,3 +19,14 @@ export const fetchShowGenreData = async () => {
     throw error;
   }
   }
+
+  export const fetchShowDetails = async (showId) => {
+    try {
+      const response = await fetch(`https://podcast-api.netlify.app/id/${showId}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Could not fetch show details for ID ${showId}:`, error);
+      throw error;
+    }
+  };
