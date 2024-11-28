478a3bc0b4faf86643986339e34dd18a28602fc5
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-28 15:10:12 +0200

    Added links and routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import './App.css'
-//import './components/ShowPreview'
-import ShowPreview from './components/ShowPreview'
-import ShowDetails from './components/ShowDetails'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AudioProvider } from './context/AudioContext';
+import { FavouritesProvider } from './context/FavouritesContext';
+
+// Page Imports
+import HomePage from './pages/HomePage';
+import ShowPage from './pages/ShowPage';
+import GenrePage from './pages/GenrePage';
+import FavouritesPage from './pages/FavouritesPage';
+
+// Component Imports
+import AudioPlayer from './components/AudioPlayer';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div>
-      <ShowPreview />
-      <ShowDetails  />
-    </div>
+    <Router>
+      <AudioProvider>
+        <FavouritesProvider>
+          <div className="app-container">
+            <Navigation />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/show/:id" element={<ShowPage />} />
+                <Route path="/genre/:genreId" element={<GenrePage />} />
+                <Route path="/favourites" element={<FavouritesPage />} />
+              </Routes>
+            </main>
+            <AudioPlayer />
+          </div>
+        </FavouritesProvider>
+      </AudioProvider>
+    </Router>
   );
 }
 
-export default App
+export default App;
