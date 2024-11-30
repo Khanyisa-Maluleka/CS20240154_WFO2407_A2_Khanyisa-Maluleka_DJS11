1e363c611b50ef9e5f7299fd2f5cc9c2cae62f34
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-30 22:11:42 +0200

    Updated the files

--- src/components/ShowDetails.jsx
+++ src/components/ShowDetails.jsx
@@ -131,7 +131,7 @@ const ShowDetails = ({ onAddToFavourites }) => {
                       className="favourite-button"
                     >
                       Favourite
-                    </button>
+                    </button>  
                   </div>
                 </div>
               );
